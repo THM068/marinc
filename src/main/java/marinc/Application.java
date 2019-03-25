3edb9d10812c1ff79182c0ad9c4b7f2e9d62e3b4
Thando Mafela <thandomafela@hotmail.com>
2019-03-25 15:43:57 +0000

    init app

---
+++ src/main/java/marinc/Application.java
@@ -0,0 +1,10 @@
+package marinc;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
